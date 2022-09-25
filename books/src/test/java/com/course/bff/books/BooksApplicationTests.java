e6f54fd009772046b8693b1f5e92d17cdd19ebc7
aozornov <andrii-lohika@abra.com>
2022-09-25 12:08:02 +0300

    system-observability-session-1

--- books/src/test/java/com/course/bff/books/BooksApplicationTests.java
+++ books/src/test/java/com/course/bff/books/BooksApplicationTests.java
@@ -1,4 +1,4 @@
-package com.course.bff.authors;
+package com.course.bff.books;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CourseServicemeshAuthorsApplicationTests {
+public class BooksApplicationTests {
 
     @Test
     public void contextLoads() {
